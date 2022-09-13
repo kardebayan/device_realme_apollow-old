4e043827445f2cfc5bb2cb3d8399770ceb52d33e
bengris32 <bengris32@protonmail.ch>
2022-09-13 18:36:46 +0100

    apollow: Build required DRM interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor \
     android.hardware.camera.provider@2.6.vendor
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3.vendor
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
