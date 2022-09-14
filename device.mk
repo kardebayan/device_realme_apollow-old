aa6e712da3cbe3a27efdb97d3b3b6b1ecb4ef498
bengris32 <bengris32@protonmail.ch>
2022-09-14 19:49:02 +0100

    apollow: Build default Composer HIDL service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I220069ddd0a8e01b646d9f8f71710102c674e30a

--- device.mk
+++ device.mk
@@ -58,6 +58,9 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 # Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-service
+
 TARGET_SCREEN_DENSITY := 440
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
