6cc323b3cae8ffcc21422aa2c6c8d5a699437077
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-09-28 19:17:19 +0100

    apollow: Enable updateable APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,9 @@
 DEVICE_PATH := device/realme/apollow
 BOARD_VENDOR := realme
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
