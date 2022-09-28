c0e0bddc1c70c010622af21b3db6ea481151f8b6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-28 19:18:01 +0100

    apollow: Enforce RRO for all overlays

--- device.mk
+++ device.mk
@@ -65,6 +65,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
