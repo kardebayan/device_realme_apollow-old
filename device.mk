53a8968bdb444fceab549da54d1feb888b197695
Bowgo Tsai <bowgotsai@google.com>
2022-10-01 15:54:47 +0100

    apollow: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger
    
    Change-Id: I0cf78bfc80c24bb217bb88831faa5a29ba04d194

--- device.mk
+++ device.mk
@@ -1,8 +1,8 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
